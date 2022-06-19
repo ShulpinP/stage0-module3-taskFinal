97f3cc53b8d4683c1b880bb3d06933df0b2a146c
ShulpinP <pavelshulpin6@gmail.com>
2022-06-20 01:26:50 +0300

    final module 1

--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,9 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+        int first = number%10;
+        int second = number/10%10;
+        int third = number/100%10;
+        System.out.println(first*100+second*10+third);
     }
 }
